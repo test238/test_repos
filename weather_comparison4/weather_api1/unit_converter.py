d74ed0b45bc4f42fe1f6038073302dca8d14e649
test238 <64161625+test238@users.noreply.github.com>
2020-05-07 00:46:56 +0200

    Add files via upload

---
+++ weather_comparison4/weather_api1/unit_converter.py
@@ -0,0 +1,25 @@
+import re
+
+def dms2dd(degrees, minutes, seconds, remainder, direction):
+    dd = float(degrees) + float(minutes)/60 + float(seconds)/(60*60);
+    if direction == 'W' or direction == 'S':
+        dd *= -1
+    return dd;
+
+def dd2dms(deg):
+    d = int(deg)
+    md = abs(deg - d) * 60
+    m = int(md)
+    sd = (md - m) * 60
+    return [d, m, sd]
+
+def parse_dms(dms):
+    parts = re.split('[^\d\w]+', dms)
+    lat = dms2dd(parts[0], parts[1], parts[2], parts[3], parts[4])
+
+    return (lat)
+
+dd = parse_dms("78°55'44.33324'N" )
+
+print(dd)
+
